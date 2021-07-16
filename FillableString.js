7ece491a65c53503ddc2e07432dd863c45d3d08b
Tayron Mîranda <contato@tayron.com.br>
2021-07-16 13:43:35 -0300

    Ajuste estrutura de classe para ECMAScript 6

--- FillableString.js
+++ FillableString.js
@@ -1,98 +1,102 @@
 'use strict';
 
 module.exports = class FillableString {
-  static POSITION_LEFT = 'left'
-  static POSITION_RIGHT = 'right'
+  static get POSITION_LEFT () {
+    return 'left'
+  }
 
-  constructor(string) {
+  static get POSITION_RIGHT () {
+    return 'right'
+  }
+
+  constructor (string) {
     this._string = string !== undefined ? string : ''
   }
 
-  fillAndCompleteWith(string, fillable, size, position) {
+  fillAndCompleteWith (string, fillable, size, position) {
     let fillableString = string
 
     if (size > 0 && position === FillableString.POSITION_LEFT) {
       fillableString = fillableString.padStart(size, fillable)
     }
 
-    if (size > 0 && position === FillableString.POSITION_RIGHT) {                
+    if (size > 0 && position === FillableString.POSITION_RIGHT) {
       fillableString = fillableString.padEnd(size, fillable)
     }
-    
+
     this._string += fillableString
   }
 
-  fill = function (string, size, position) {
-
-    if (size > 0 & !position) {                
-        this._concatRight(string, size)      
+  fill (string, size, position) {
+    if (size > 0 & !position) {
+      this._concatRight(string, size)
     }
 
     if (size > 0 && position === FillableString.POSITION_LEFT) {
       this._concatLeft(string, size)
     }
 
-    if (size > 0 && position === FillableString.POSITION_RIGHT) {                
+    if (size > 0 && position === FillableString.POSITION_RIGHT) {
       this._concatRight(string, size)
-    }    
+    }
 
     if (!size && !position && this._string !== undefined) {
       this._string += string
     }
   }
 
-  completeWith = function(string, size, position) {
+  completeWith (string, size, position) {
     if (size > 0 && position === FillableString.POSITION_LEFT) {
       this._string = this._string.padStart(size, string)
     }
 
-    if (size > 0 && position === FillableString.POSITION_RIGHT) {                
-      this._string = this._string.padEnd(size, string)      
-    } 
+    if (size > 0 && position === FillableString.POSITION_RIGHT) {
+      this._string = this._string.padEnd(size, string)
+    }
   }
 
-  insertBefore = function(stringToFind, stringToAdd) {
+  insertBefore (stringToFind, stringToAdd) {
     const stringToReplace = stringToAdd + stringToFind
     if (this._string !== '') {
       this._string = this._string.replace(stringToFind, stringToReplace)
     }
   }
 
-  insertAfter = function(stringToFind, stringToAdd) {
+  insertAfter (stringToFind, stringToAdd) {
     const stringToReplace = stringToFind + stringToAdd
     if (this._string !== '') {
       this._string = this._string.replace(stringToFind, stringToReplace)
     }
   }
 
-  getString = function () {
+  getString () {
     return this._string
   }
 
-  _concatRight = function (string, size) {
-    for (var i = 0; i <= size -1; i++) {
+  _concatRight (string, size) {
+    for (var i = 0; i <= size - 1; i++) {
       this._string += string
     }
   }
 
-  _concatLeft = function (string, size) {
+  _concatLeft (string, size) {
     let text = ''
-    for (var i = 0; i <= size -1; i++) {
+    for (var i = 0; i <= size - 1; i++) {
       text += string
     }
 
     this._string = text + this._string
-  }  
-
-  removeSpecialCharacter = function() {    
-    this._string = this._string.replace(/[^a-zA-Z0-9 ]/g, "")
   }
 
-  removeWhiteSpaces = function() {
-    this._string = this._string.replace(/\s/g,'')
-  }  
+  removeSpecialCharacter () {
+    this._string = this._string.replace(/[^a-zA-Z0-9 ]/g, '')
+  }
 
-  getByPosition = function(start, end) {    
-    return this._string.substring((start-1), end);
+  removeWhiteSpaces () {
+    this._string = this._string.replace(/\s/g, '')
+  }
+
+  getByPosition (start, end) {
+    return this._string.substring((start - 1), end);
   }
 }
